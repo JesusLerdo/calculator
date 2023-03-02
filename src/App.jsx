ccfd8d6c4a1992c98f62e96ced4e3a02dc9f9a96
Jesus Lerdo Atilano <isc19350300@gmail.com>
2023-03-01 18:12:47 -0600

    firstcalculator

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+function App() {
+  return (
+    <div>
+  <h1>Calculator</h1>
+  <h1 />
+  <table style={{
+     width: '400px'
+     }}>
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+
+   <tr style={{border: '1px solid black',}}>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   <td style={{border: '1px solid black',}}>0</td>
+   </tr>
+   
+  </table>
+</div>
+  )
+}  
+
+export default App;
