a94c8806f0b59433297bf55e319063d991ffe448
Jesus Lerdo Atilano <isc19350300@gmail.com>
2023-03-08 16:52:19 -0600

    Calculator3

--- src/App.jsx
+++ src/App.jsx
@@ -12,48 +12,48 @@ function App() {
    </tr>
    <tr>
    <td><button type='button'
-   className='btn btn-primary'> C </button></td>
+   className='btn btn-primary w-75 mt-2'> C </button></td>
    <td><button type='button'
-   className='btn btn-primary'> / </button></td>
+   className='btn btn-primary w-75 mt-2'> / </button></td>
    <td><button type='button'
-   className='btn btn-primary'> * </button></td>
+   className='btn btn-primary w-75 mt-2'> * </button></td>
    <td><button type='button'
-   className='btn btn-primary'> - </button></td>
+   className='btn btn-primary w-75 mt-2'> - </button></td>
    </tr>
    <tr>
    <td><button type='button'
-   className='btn btn-primary'> 7 </button></td>
+   className='btn btn-primary w-75 mt-2'> 7 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 8 </button></td>
+   className='btn btn-primary w-75 mt-2'> 8 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 9 </button></td>
+   className='btn btn-primary w-75 mt-2'> 9 </button></td>
    <td rowSpan={2}><button type='button'
-   className='btn btn-primary'> + </button></td>
+   className='btn btn-primary w-75 mt-2' style={{height: "85px"}}> + </button></td>
    </tr>
    <tr>
    <td><button type='button'
-   className='btn btn-primary'> 4 </button></td>
+   className='btn btn-primary w-75 mt-2'> 4 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 5 </button></td>
+   className='btn btn-primary w-75 mt-2'> 5 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 6 </button></td>
+   className='btn btn-primary w-75 mt-2'> 6 </button></td>
    </tr>
    <tr>
    <td><button type='button'
-   className='btn btn-primary'> 1 </button></td>
+   className='btn btn-primary w-75 mt-2'> 1 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 2 </button></td>
+   className='btn btn-primary w-75 mt-2'> 2 </button></td>
    <td><button type='button'
-   className='btn btn-primary'> 3 </button></td>
+   className='btn btn-primary w-75 mt-2'> 3 </button></td>
    <td rowSpan={2}><button type='button'
-   className='btn btn-primary'> = </button></td>
+   className='btn btn-primary w-75 mt-2' style={{height: "85px"}} > = </button></td>
    </tr>
    <tr>
    <td colSpan={2}><button type='button'
-   className='btn btn-primary'> 0 </button></td>
+   className='btn btn-primary' style={{width: "175px"}}> 0 </button></td>
    <td>
    <button type='button'
-   className='btn btn-primary'> . </button>
+   className='btn btn-primary w-75 mt-2'> . </button>
    </td>
    </tr>
    
